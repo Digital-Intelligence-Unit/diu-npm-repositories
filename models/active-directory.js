ad632180a6c9d2942da8e870260ea2000653f17a
Stewart Morgan <stewartmorgan86@gmail.com>
2022-05-12 21:04:33 +0100

    removed bug

--- models/active-directory.js
+++ models/active-directory.js
@@ -1,6 +1,6 @@
-const BaseModel = require("./base/secrets");
-const ADClient = require("activedirectory");
-const Aws = require("./helpers/aws");
+const BaseModel = require("./base/dynamo-db");
+// const ADClient = require("activedirectory");
+// const Aws = require("./helpers/aws");
 
 class ActiveDirectory extends BaseModel {
     tableName = "credentials";
@@ -10,72 +10,73 @@ class ActiveDirectory extends BaseModel {
     }
 
     getInstance(name, callback) {
+        callback(new Error("Not implemented. Moved to API codebase due to configuration of secrets issues"), null);
         // Get the settings from dynamodb
-        this.getByKeys({ type: "ActiveDirectory", name }, (err, result) => {
-            // Errors
-            if (err) {
-                callback(err, null);
-                return;
-            }
-            if (result.Items.length === 0) {
-                const errResponse = "Unknown organisation";
-                callback(errResponse, null);
-                return;
-            }
+        // this.getByKeys({ type: "ActiveDirectory", name }, (err, result) => {
+        //     // Errors
+        //     if (err) {
+        //         callback(err, null);
+        //         return;
+        //     }
+        //     if (result.Items.length === 0) {
+        //         const errResponse = "Unknown organisation";
+        //         callback(errResponse, null);
+        //         return;
+        //     }
 
-            // Decrypt settings
-            const item = result.Items[0];
-            Aws.getSecretsAsync(item.name, (errGetSecret, data) => {
-                if (errGetSecret) {
-                    callback(errGetSecret, null);
-                } else {
-                    // Return active directory instance
-                    callback(
-                        null,
-                        new ADClient(
-                            Object.assign(
-                                {
-                                    attributes: {
-                                        user: [
-                                            "manager",
-                                            "distinguishedName",
-                                            "userPrincipalName",
-                                            "sAMAccountName",
-                                            "mail",
-                                            "lockoutTime",
-                                            "whenCreated",
-                                            "pwdLastSet",
-                                            "userAccountControl",
-                                            "employeeID",
-                                            "sn",
-                                            "givenName",
-                                            "initials",
-                                            "cn",
-                                            "displayName",
-                                            "comment",
-                                            "description",
-                                            "linemanager",
-                                            "objectSid",
-                                        ],
-                                        group: ["distinguishedName", "objectCategory", "cn", "description"],
-                                    },
-                                    entryParser(entry, raw, innerCallback) {
-                                        if (raw.hasOwnProperty("objectGUID")) {
-                                            entry.objectGUID = raw.objectGUID;
-                                        }
-                                        if (raw.hasOwnProperty("objectSid")) {
-                                            entry.objectSid = raw.objectSid;
-                                        }
-                                        innerCallback(entry);
-                                    },
-                                },
-                                data
-                            )
-                        )
-                    );
-                }
-            });
-        });
+        //     // Decrypt settings
+        //     const item = result.Items[0];
+        //     Aws.getSecretsAsync(item.name, (errGetSecret, data) => {
+        //         if (errGetSecret) {
+        //             callback(errGetSecret, null);
+        //         } else {
+        //             // Return active directory instance
+        //             callback(
+        //                 null,
+        //                 new ADClient(
+        //                     Object.assign(
+        //                         {
+        //                             attributes: {
+        //                                 user: [
+        //                                     "manager",
+        //                                     "distinguishedName",
+        //                                     "userPrincipalName",
+        //                                     "sAMAccountName",
+        //                                     "mail",
+        //                                     "lockoutTime",
+        //                                     "whenCreated",
+        //                                     "pwdLastSet",
+        //                                     "userAccountControl",
+        //                                     "employeeID",
+        //                                     "sn",
+        //                                     "givenName",
+        //                                     "initials",
+        //                                     "cn",
+        //                                     "displayName",
+        //                                     "comment",
+        //                                     "description",
+        //                                     "linemanager",
+        //                                     "objectSid",
+        //                                 ],
+        //                                 group: ["distinguishedName", "objectCategory", "cn", "description"],
+        //                             },
+        //                             entryParser(entry, raw, innerCallback) {
+        //                                 if (raw.hasOwnProperty("objectGUID")) {
+        //                                     entry.objectGUID = raw.objectGUID;
+        //                                 }
+        //                                 if (raw.hasOwnProperty("objectSid")) {
+        //                                     entry.objectSid = raw.objectSid;
+        //                                 }
+        //                                 innerCallback(entry);
+        //                             },
+        //                         },
+        //                         data
+        //                     )
+        //                 )
+        //             );
+        //         }
+        //     });
+        // });
     }
 }
 
