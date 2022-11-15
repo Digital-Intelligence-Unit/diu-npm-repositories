8d5b04ed3b8d5e28d408fe8fee70eacee5d6334f
luket-nhs <luke.tinnion@nhs.net>
2022-11-15 16:06:46 +0000

    Ammendments for dynamic queries

--- models/pbi-metric-data.js
+++ models/pbi-metric-data.js
@@ -4,23 +4,32 @@ class PBIMetricData extends BaseModel {
 
     getByMetricId(id, filters = { level: "ward" }, callback) {
         // Select all
-        this.query(
-            {
-                text: `
-                    SELECT ${this.tableName}.*, pbi_geographies.geo_name, ST_AsGeoJSON(pbi_geographies.geom) as geojson
-                    FROM ${this.tableName} 
-                    RIGHT JOIN pbi_geographies ON pbi_geographies.geo_id = ${this.tableName}.geo_id
-                    WHERE ${this.tableName}.metric_level_id = (
-                        SELECT metric_level_id from pbi_metrics_level 
-                        WHERE metric_id = $1 AND metric_level = $2
-                    );
-                `,
-                values: [id, filters.level],
-            },
-            (err, result) => {
-                callback(err, result);
-            }
-        );
+        const query = {
+            text: `
+                SELECT ${this.tableName}.*, pbi_geographies.geo_name, ST_AsGeoJSON(pbi_geographies.geom) as geojson
+                FROM ${this.tableName} 
+                RIGHT JOIN pbi_geographies ON pbi_geographies.geo_id = ${this.tableName}.geo_id
+                WHERE ${this.tableName}.metric_level_id = (
+                    SELECT metric_level_id from pbi_metrics_level 
+                    WHERE metric_id = $1 AND metric_level = $2
+                ) AND metric_period = $3`,
+            values: [id, filters.level, filters.period],
+        };
+
+        // Dynamic filter?
+        if (filters.value_operator && filters.value) {
+            // Note: Make sure to validate operator first
+            query.text += ` AND (
+                metric_data_value_float ${filters.value_operator} $4 OR 
+                metric_data_value_float ${filters.value_operator} $4
+            )`;
+            query.values.push(filters.value);
+        }
+
+        // Make query and return
+        this.query(query, (err, result) => {
+            callback(err, result);
+        });
     }
 }
 
