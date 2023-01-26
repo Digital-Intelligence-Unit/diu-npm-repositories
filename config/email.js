481621ad4bec0119d3a4c58650fc84bdef36b25d
luket-nhs <luke.tinnion@nhs.net>
2023-01-26 15:46:55 +0000

    Initial changes

--- config/email.js
+++ config/email.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const site = process.env.SITE_URL || "dev.nexusintelligencenw.nhs.uk";
 
 module.exports = {
@@ -10,70 +11,132 @@ module.exports = {
         team_request_action: "https://api." + site + "/teamrequests/action",
     },
     message_template: `
-        <style>
-            @import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
-            div { width: calc(100% - 30px); }
-            .full { margin: 15px; }
-            .body { background-color: #eef5f9; width: 100%; padding: 10px 0px; }
-            .header { background-color: #27205d; padding: 10px 5px; text-align: center;  }
-            .main { font-family: "Poppins", sans-serif !important; }
-            .footer { text-align: center; }
-            .mat-card {
-                transition: box-shadow 280ms cubic-bezier(.4,0,.2,1);
-                display: block;
-                position: relative;
-                padding: 16px;
-                border-radius: 4px;
-                background-color: white;
-            }
-            .mat-button {
-                border-radius: 5px;
-                font-family: "Poppins", sans-serif !important;
-                box-sizing: border-box;
-                position: relative;
-                -webkit-user-select: none;
-                -moz-user-select: none;
-                -ms-user-select: none;
-                user-select: none;
-                cursor: pointer;
-                outline: 0;
-                border: none;
-                -webkit-tap-highlight-color: transparent;
-                display: inline-block;
-                white-space: nowrap;
-                text-decoration: none;
-                vertical-align: baseline;
-                text-align: center;
-                margin: 2px 10px;
-                min-width: 64px;
-                line-height: 36px;
-                padding: 0 16px;
-                border-radius: 4px;
-                overflow: visible;
-                transform: translate3d(0,0,0);
-                transition: background .4s cubic-bezier(.25,.8,.25,1),box-shadow 280ms cubic-bezier(.4,0,.2,1);
-                color: white;
-            }
-            .action {
-                text-align: center;
-            }
-            .primary {
-                background-color: #1e88e5;
-            }
-            .accent {
-                background-color: #3f51b5;
-            }
-            .warn {
-                background-color: #e91e63;
-            }
-            .default {
-                color: black !important;
-                background-color: white;
-                border: 1px solid darkgray;
-            }
-        </style>
-        <div class="body">
-            <div class="full main mat-card">MESSAGE</div>
-        </div>
+    <!doctype html>
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>Nexus Intelligence</title>
+            <style>
+                /* -------------------------------------
+                    MAIN TEMPLATE
+                ------------------------------------- */
+                body {
+                    font-family: "Poppins", sans-serif !important;
+                    background-color: #eef5f9;
+                    font-family: sans-serif;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                table {
+                    border-collapse: separate;
+                    mso-table-lspace: 0pt;
+                    mso-table-rspace: 0pt;
+                    width: 100%;
+                }
+
+                table td {
+                    vertical-align: top;
+                }
+
+                .body {
+                    background-color: #eef5f9;
+                    width: 100%;
+                }
+
+                /* Set a max-width, and make it display as block so it will automatically stretch to that width, 
+                but will also shrink down on a phone or something */
+                .container {
+                    display: block;
+                    margin: 0 auto !important;
+                    /* makes it centered */
+                    max-width: 580px;
+                    padding: 10px;
+                    width: 580px;
+                }
+
+                /* This should also be a block element, so that it will fill 100% of the .container */
+                .content {
+                    box-sizing: border-box;
+                    display: block;
+                    margin: 0 auto;
+                    max-width: 580px;
+                    padding: 10px;
+                }
+
+                .main {
+                    background: #ffffff;
+                    border-radius: 3px;
+                }
+
+                .wrapper {
+                    box-sizing: border-box;
+                    padding: 20px;
+                }
+
+                /* -------------------------------------
+                    RESPONSIVE AND MOBILE FRIENDLY STYLES
+                ------------------------------------- */
+                @media only screen and (max-width: 620px) {
+                    table.body .content {
+                        padding: 0 !important;
+                    }
+
+                    table.body .container {
+                        padding: 0 !important;
+                        width: 100% !important;
+                    }
+
+                    table.body .main {
+                        border-left-width: 0 !important;
+                        border-radius: 0 !important;
+                        border-right-width: 0 !important;
+                    }
+                }
+
+                /* -------------------------------------
+                    CUSTOM STYLE
+                ------------------------------------- */
+                .logo {
+                    text-align: center;
+                    background-color: #27205d;
+                    padding: 15px 0px;
+                    margin-bottom: 30px;
+                }
+            </style>
+        </head>
+
+        <body>
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+                <tr>
+                    <td>&nbsp;</td>
+                    <td class="container">
+                        <!-- START CENTERED WHITE CONTAINER -->
+                        <table role="presentation" class="main" width="580px">
+
+                            <!-- START MAIN CONTENT AREA -->
+                            <tr>
+                                <td class="wrapper">
+                                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                                        <tr>
+                                            <td>
+                                                <div class="logo">
+                                                    <img width="280px" src="https://www.nexusintelligencenw.nhs.uk/assets/images/nexusgenNHS.png">
+                                                </div>
+
+                                                <p>MESSAGE</p>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </body>
+    </html>
     `,
 };
