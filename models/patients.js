8362c2a508b460e9d66e71763a7cf3a4109c351b
alexseymourdev <alexseymourdev@gmail.com>
2023-04-26 18:21:12 +0100

    Update patients.js

--- models/patients.js
+++ models/patients.js
@@ -7,7 +7,7 @@ class PatientsModel extends BaseModel {
     selectjoin = `SELECT M.sex,M.age,M.risk_score_int,M.asthma,M.copd,M.chd,M.heart_failure,
     M.hypertension,M.atrial_fibrillation,M.pad,M.cancer,M.depression,M.dementia,M.mental_health,M.learning_disabilities,M.diabetes,
     M.hypothyroid,M.ckd,M.epilepsy,M.osteoporosis,M.rheumatoid_arthritis,M.execution_date, M.testdigest, M.wellbeing_acorn_type,
-    M.household_group,M.household_type,M.mosaic_label FROM population_master as M`;
+    M.household_group,M.household_type,M.mosaic_label,M.gpp_code FROM population_master as M`;
 
     getAll(limit, roles, callback) {
         const rolecheck = PermissionsHelper.capabilitiesAsSqlQuery(roles, "populationjoined", "M", true);
@@ -84,6 +84,7 @@ class PatientsModel extends BaseModel {
             });
         } else {
             const query = this.selectjoin + " WHERE " + rolecheck + " testdigest = $1 LIMIT 1";
+            console.log(query);
             this.query({ text: query, values: [digest] }, (error, results) => {
                 if (error) {
                     console.log("Error: " + error);
