f11570eddbe6400e29ab0b6113209c08e8c16471
Luke's Tinnion <103183269+luket-nhs@users.noreply.github.com>
2024-11-11 17:11:04 +0000

    Added getCredentialsMethod

--- src/helpers/aws.ts
+++ src/helpers/aws.ts
@@ -2,9 +2,14 @@ import { fromEnv, fromSSO } from "@aws-sdk/credential-providers";
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager'
 
 export class AWSHelper {
+    static getCredentials() {
+        // To do: Change to fromTemporaryCredentials()
+        return (!process.env.NODE_ENV || process.env.NODE_ENV == 'local') ? fromSSO() : fromEnv();
+    }
+
     static async getSecret(id: string): Promise<string> {
-        const client = new SecretsManagerClient({ 
-            credentials: (!process.env.NODE_ENV || process.env.NODE_ENV == 'local') ? fromSSO() : fromEnv()
+        const client = new SecretsManagerClient({
+            credentials: AWSHelper.getCredentials()
         });
 
         const secret = await client.send(
